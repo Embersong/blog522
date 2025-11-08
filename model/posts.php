003984855e984ef74422ee8485b0902d4ca26756
Embersong <evebay@mail.ru>
2025-11-08 18:32:26 +0700

    post-create adn calc

--- model/posts.php
+++ model/posts.php
@@ -1,5 +1,28 @@
 <?php
 
+function createPost(string $title, string $text, int $category_id): int
+{
+    $posts = getPosts();
+
+    //создаем новый пост в массиве, id генерится автоматом
+    $posts[] = [
+        'category_id' => $category_id,
+        'title' => $title,
+        'text' => $text,
+    ];
+
+    //добавляем сгенерированный id в массив
+    $lastKey = array_key_last($posts);
+    $posts[$lastKey]['id'] = $lastKey;
+
+    //сделать красиво, чтобы id шел первым
+    $posts[$lastKey] = array_merge(['id' => $lastKey], $posts[$lastKey]);
+
+    file_put_contents(ROOT_PATH . '/posts.json', json_encode($posts, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+
+    return $lastKey;
+}
+
 function getPost(int $id): ?array
 {
     return getPosts()[$id] ?? null;
--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ include __DIR__ . '/../config/app.php';
 
 $page = $_GET['page'] ?? 'index';
 
+
 switch ($page) {
     case 'index':
         echo render('index');
@@ -18,8 +19,8 @@ switch ($page) {
         if (!empty($_POST)) {
             $arg1 = (float)($_POST['arg1'] ?? 0);
             $arg2 = (float)($_POST['arg2'] ?? 0);
-
-            $result = $arg1 + $arg2;
+            $operation = $_POST['operation'] ?? '';
+            $result = calculate($arg1, $arg2, $operation);
         }
         echo render('calc', [
             'arg1' => $arg1,
@@ -31,7 +32,9 @@ switch ($page) {
     case 'categories':
         $categories = getCategories();
 
-        include VIEWS_PATH . '/categories/index.phtml';
+        echo render('categories/index', [
+            'categories' => $categories,
+        ]);
         break;
 
     case 'posts-by-category':
@@ -56,11 +59,42 @@ switch ($page) {
         break;
 
     case 'post':
+        $success = $_GET['success'] ?? null;
         $id = (int)($_GET['id'] ?? 0);
         $post = getPost($id);
 
         echo render('posts/show', [
             'post' => $post,
+            'success' => $success,
+        ]);
+        break;
+
+    case 'post-create':
+        $categories = getCategories();
+        $success = $_GET['success'] ?? null;
+
+        if (!empty($_POST)) {
+
+            //добавляете пост в файл
+
+            $title = htmlspecialchars($_POST['title']);
+            $text = htmlspecialchars($_POST['text']);
+            $category_id = (int)$_POST['category_id'];
+
+            //Валидация
+            if (empty($title) || empty($text) || empty($category_id)) {
+                header('Location: ?page=post-create&success=0');
+                die();
+            }
+            $lastKey = createPost($title, $text, $category_id);
+
+            //редирект методом GET
+            header('Location: ?page=post&id=' . $lastKey . '&success=1');
+            die();
+        }
+        echo render('posts/create', [
+            'categories' => $categories,
+            'success' => $success,
         ]);
         break;
 
